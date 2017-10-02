e1642e5ee6b4b9728fa2313e5f5e064307a077d7
Ultan <ultankearns@gmail.com>
2017-10-02 09:47:41 +0100

    Initial text editor files
    Basic GUI layout need to add features such as: the ability to save/ read files, replace words and change font size.

---
+++ TextEditor/src/Gui.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Gui extends JFrame{
+	
+	public Gui()
+	{
+		setTitle("Text Editor");
+		setSize(800,600);
+		setVisible(true);
+		setResizable(false);
+	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
---
+++ TextEditor/src/TextEditor.java
@@ -0,0 +1,9 @@
+
+public class TextEditor {
+
+	public static void main(String[] args) {
+		Gui GUI = new Gui();
+
+	}
+
+}
