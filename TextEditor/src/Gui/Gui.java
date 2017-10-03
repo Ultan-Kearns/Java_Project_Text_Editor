681314d1f267f83f2b2ef175bc524ceb11f0ad10
Ultan <ultankearns@gmail.com>
2017-10-03 16:37:39 +0100

    Added some new features
    Need to fix scrollbar but added new menu items need to provide functionality for them.

--- TextEditor/src/Gui/Gui.java
+++ TextEditor/src/Gui/Gui.java
@@ -2,26 +2,38 @@
 package Gui;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class Gui extends JFrame  //text editor gui
 {
-	
+	private static final long serialVersionUID = 1L;
+
 	public Gui() //constructor
 	{
 		setTitle("Text Editor");
 		setSize(800,600);
 		setResizable(true);
 		setLayout(new FlowLayout());
-		JTextArea text = new JTextArea(200,70);
-		JMenuBar menubar = new JMenuBar();
-		JScrollBar horizontal = new JScrollBar();
+		JTextArea text = new JTextArea(20,20);
+		JMenuBar menu = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenu edit = new JMenu("Edit");
+		JScrollPane scroll = new JScrollPane(text);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		text.setEditable(true);
+		file.add("New File");
+		file.add("Rename");
+		file.add("Delete");
+		edit.add("Replace");
+		menu.add(file);
+		menu.add(edit);
 		add(text);
-		add(menubar);
-		add(horizontal);
+		add(scroll);
+		setJMenuBar(menu);
+		setResizable(false);
 		setVisible(true); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
