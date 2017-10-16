c6effbbbf5b996b198a0b3ca9497560f0bfd5cf1
Ultan <ultankearns@gmail.com>
2017-10-16 16:10:15 +0100

    Save function implemented
    Added save functionality

--- TextEditor/src/GUI.java
+++ TextEditor/src/GUI.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.PrintWriter;
 
 import javax.swing.JMenu;
@@ -12,14 +13,16 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class GUI extends TextEditor {
+
+public class GUI extends TextEditor{
+	JTextArea text = new JTextArea(800,600);
+	 
 	public GUI() //constructor
 	{
 		JPanel pane = new JPanel();
 		setTitle("Text Editor");
 		setSize(800,600);
 		pane.setLayout(new GridLayout());
-		JTextArea text = new JTextArea(20,20);
 		JMenuBar menu = new JMenuBar();
 		JMenu file = new JMenu("File");
 		JMenu edit = new JMenu("Edit");
@@ -31,11 +34,13 @@ public class GUI extends TextEditor {
 		text.setEditable(true); //Allow user to edit text
 		text.setLineWrap(true); //Set so text moves to new line when reaches end
 		text.setWrapStyleWord(true);
-		JMenuItem newFile = new JMenuItem("New File");
-		JMenuItem rename = new JMenuItem("Rename"); //Turn these to JMenuItems to implement action
-		file.add("Delete");
-		file.add("Save");
+		JMenuItem delete = new JMenuItem("Delete");
+		JMenuItem save = new JMenuItem("Save");
+		file.add(delete);
+		file.add(save);
 		edit.add("Replace");
+		saveFile saveF = new saveFile();
+		save.addActionListener(saveF);
 		menu.add(file);
 		menu.add(edit);
 		pane.add(scroll,BorderLayout.CENTER);
@@ -52,16 +57,24 @@ public class GUI extends TextEditor {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			//allow user to enter file name and append with .txt
 			String s = JOptionPane.showInputDialog("Input name of file:");
+			//try catch block
 			try
 			{
-				PrintWriter file = new PrintWriter(s + ".txt");
+				//Create new file
+				File userFile =  new File("C:/" + s + ".txt"); 
+				PrintWriter file = new PrintWriter(userFile); 
+				file.println(text.getText());
+				JOptionPane.showMessageDialog(null,"File created and saved in C:\\ drive");
 				file.close();
 			}
+			//if failure occurs
 			catch(Exception file)
 			{
-				System.out.println("File not created\n"
+				JOptionPane.showMessageDialog(null,"File not created\n"
 						+ "Please check to see if file name entered already exists");
+				file.printStackTrace();
 			}
 		}
 	}
